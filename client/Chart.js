4efb5d0bd395c05d52401a17d6369f8dbade8820
Eric Feinstein <erichfeinstein@gmail.com>
2019-03-20 10:49:22 -0400

    Feature: first iteration of data visualization with react-d3-components

--- client/Chart.js
+++ client/Chart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { PieChart } from 'react-d3-components';
 
 //Socket config
 const IP = 'http://localhost:3000';
@@ -17,11 +18,16 @@ export default class Chart extends React.Component {
   }
   componentDidMount() {
     this.socket.on('tweet', score => {
+      //Ignore true neutral tweets
+      if (score === 0) return;
+
+      let scorePhrase = calculatePhrase(score);
       let scores = this.state.scores;
-      let oldScoreCount = scores[score];
+      let oldScoreCount = scores[scorePhrase];
       if (!oldScoreCount) oldScoreCount = 1;
       else oldScoreCount++;
-      scores[score] = oldScoreCount;
+
+      scores[scorePhrase] = oldScoreCount;
       this.setState({
         scores,
       });
@@ -29,12 +35,33 @@ export default class Chart extends React.Component {
   }
 
   render() {
+    const data = {
+      label: 'Chart',
+      values: Object.entries(this.state.scores).map(score => {
+        return { x: score[0], y: score[1] };
+      }),
+    };
     return (
       <div>
-        {Object.keys(this.state.scores).map((score, i) => {
-          return <div key={i}>{score}</div>;
-        })}
+        <PieChart
+          data={data}
+          width={1000}
+          height={700}
+          margin={{ top: 10, bottom: 10, left: 100, right: 100 }}
+        />
       </div>
     );
   }
+}
+
+function calculatePhrase(score) {
+  //Negatives
+  if (score <= -10) return 'Overwhelmingly Negative';
+  else if (score < -5 && score >= -10) return 'Mostly Negative';
+  else if (score < 0 && score >= -5) return 'Negative';
+  //Positives
+  else if (score > 0 && score <= 5) return 'Positive';
+  else if (score > 5 && score <= 10) return 'Mostly Positive';
+  else if (score >= 10) return 'Overwhelmingly Positive';
+  else return score;
 }
